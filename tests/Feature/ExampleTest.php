fd0d32d2597cf5c80c1a027564e85f7479aed4d4
jameswilddev <jameswilddev@outlook.com>
2021-04-21 12:37:04 +0100

    Prove that tokens are deleted.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -46,11 +47,16 @@ class ExampleTest extends TestCase
     {
         $token = $this->postJson('/api/login')->json('token');
 
+        var_dump(null);
+        var_dump('Tokens BEFORE logout: '.implode(', ', DB::table('personal_access_tokens')->pluck('id')->all()));
+
         $response = $this->postJson('/api/logout', [], [
             'Authorization' => 'Bearer '.$token,
         ]);
 
         $response->assertSuccessful();
+
+        var_dump('Tokens AFTER logout: '.implode(', ', DB::table('personal_access_tokens')->pluck('id')->all()));
 
         $response = $this->getJson('/api/user', [
             'Authorization' => 'Bearer '.$token,
